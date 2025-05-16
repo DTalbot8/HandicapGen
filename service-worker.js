b13c14352885c2f6be74b721ebc6ea8274981f56
DTalbot8 <167421510+DTalbot8@users.noreply.github.com>
2025-05-16 18:49:29 +0100

    Update

---
+++ service-worker.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'handicap-cache-v1';
+const FILES_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './style.css',      // if you split out CSS
+  './script.js',      // or inline your JS here
+  './icons/DDGolf.png',
+  './icons/DDGolf.png'
+];
+
+self.addEventListener('install', evt => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', evt => {
+  evt.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request)
+      .then(resp => resp || fetch(evt.request))
+  );
+});
